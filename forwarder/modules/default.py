c0f13d49fc712ecfbe74eabf85855c41485c687e
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-11-23 21:35:55 +0530

    Update default.py

--- forwarder/modules/default.py
+++ forwarder/modules/default.py
@@ -1,52 +1,222 @@
 from telegram import Update
 from telegram.ext import ContextTypes, CommandHandler, filters
 from telegram.constants import ParseMode
-
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from forwarder import bot, OWNER_ID
 
-PM_START_TEXT = """
-Hey {}, I'm {}!
-I'm a bot used to forward messages from one chat to another.
+START = """
+<b>Hi 👋🏻 {},
+I'm {} to Maintain Your Channels. I am very useful for the Channel Admin who have many Channels.
 
-To obtain a list of commands, use /help.
+See /help for more Details.
+
+Maintained By :- <a href='https://t.me/TG_Karthik'>Karthik</a></b>
 """
 
-PM_HELP_TEXT = """
-Here is a list of usable commands:
- - /start : Starts the bot.
- - /help : Sends you this help message.
-
-just send /id in private chat/group/channel and i will reply it's id.
+GROUP = """
+<b>Hi 👋🏻 {},
+I'm Working Only on Bot PM 👉🏻 <a href='https://t.me/Auto_Forward_Star_Bot'>{}</a></b>
 """
 
+HELP = """
+<b>Hi 👋🏻 {},
+
+Here is a list of usable Commands :-
+♦️ /start :- Check if 😊 I'm Alive
+♦️ /forward :- to Request to add Source And Distinction Channels ID (Direct Request to Admin)
+♦️ /help :- This Bot's Features 
+♦️ /about :- to Know About Me 😁
+♦️ /id :- Get Your 🆔
+
+Just Send /id in Private Chat/Group/Channel and i will Reply it's ID.</b>
+"""
+
+ABOUT = """
+<b>🤖 My Name :- <a href='https://t.me/Auto_Forward_Star_Bot'>{}</a>
+
+🧑🏻‍💻 Developer :- Karthik
+
+🧑🏻‍🤝‍🧑🏻 My Best Friend :- {}
+
+📝 Language :- Pyrogram
+
+📚 Framework :- Python3
+
+📡 Hosted on :- VPS
+
+📢 Updates Channel :- @Star_Moviess_Tamil</b>
+"""
+
+FORWARD = """
+<b>Hello 👋🏻 {}
+Request Your Forward Channels</b>
+"""
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat = update.effective_chat
     message = update.effective_message
     user = update.effective_user
+    keyboard = [
+        [
+            InlineKeyboardButton("Update Channel", url="https://t.me/Star_Bots_Tamil"),
+            InlineKeyboardButton("Add My Channels", url="https://t.me/TG_Karthik")
+        ]
+    ]
+    keyboard1 = [
+        [
+            InlineKeyboardButton("Check Bot PM", url="https://t.me/Auto_Forward_Star_Bot"),
+            InlineKeyboardButton("Add My Channels", url="https://t.me/TG_Karthik")
+        ],
+        [
+            InlineKeyboardButton("Update Channel", url="https://t.me/Star_Bots_Tamil")
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    reply_markup1 = InlineKeyboardMarkup(keyboard1)
     if not (chat and message and user):
         return
 
     if chat.type == "private":
         await message.reply_text(
-            PM_START_TEXT.format(user.first_name, context.bot.first_name),
+            START.format(user.first_name, context.bot.first_name),
             parse_mode=ParseMode.HTML,
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=reply_markup
         )
     else:
-        await message.reply_text("I'm up and running!")
+        await message.reply_text(
+            GROUP.format(user.first_name, context.bot.first_name),
+            parse_mode=ParseMode.HTML,
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=reply_markup1
+        )
 
-
-async def help(update: Update, _):
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat = update.effective_chat
+    user = update.effective_user
     message = update.effective_message
+    keyboard = [
+        [
+            InlineKeyboardButton("Update Channel", url="https://t.me/Star_Bots_Tamil"),
+            InlineKeyboardButton("Add My Channels", url="https://t.me/TG_Karthik")
+        ]
+    ]
+    keyboard1 = [
+        [
+            InlineKeyboardButton("Check Bot PM", url="https://t.me/Auto_Forward_Star_Bot"),
+            InlineKeyboardButton("Add My Channels", url="https://t.me/TG_Karthik")
+        ],
+        [
+            InlineKeyboardButton("Update Channel", url="https://t.me/Star_Bots_Tamil")
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    reply_markup1 = InlineKeyboardMarkup(keyboard1)
     if not (chat and message):
         return
 
     if not chat.type == "private":
-        await message.reply_text("Contact me via PM to get a list of usable commands.")
+        await message.reply_text(
+            GROUP.format(user.first_name, context.bot.first_name),
+            parse_mode=ParseMode.HTML,
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=reply_markup1
+        )
     else:
-        await message.reply_text(PM_HELP_TEXT)
+        await message.reply_text(
+            HELP.format(user.first_name, context.bot.first_name),
+            parse_mode=ParseMode.HTML,
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=reply_markup
+        )
 
+async def about(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat = update.effective_chat
+    message = update.effective_message
+    user = update.effective_user
+    keyboard = [
+        [
+            InlineKeyboardButton("Update Channel", url="https://t.me/Star_Bots_Tamil"),
+            InlineKeyboardButton("Add My Channels", url="https://t.me/TG_Karthik")
+        ]
+    ]
+    keyboard1 = [
+        [
+            InlineKeyboardButton("Check Bot PM", url="https://t.me/Auto_Forward_Star_Bot"),
+            InlineKeyboardButton("Add My Channels", url="https://t.me/TG_Karthik")
+        ],
+        [
+            InlineKeyboardButton("Update Channel", url="https://t.me/Star_Bots_Tamil")
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    reply_markup1 = InlineKeyboardMarkup(keyboard1)
+    if not (chat and message and user):
+        return
 
-bot.add_handler(CommandHandler("start", start, filters=filters.User(OWNER_ID)))
-bot.add_handler(CommandHandler("help", help, filters=filters.User(OWNER_ID)))
+    if chat.type == "private":
+        await message.reply_text(
+            ABOUT.format(context.bot.first_name, user.mention_html()),
+            parse_mode=ParseMode.HTML,
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=reply_markup
+        )
+    else:
+        await message.reply_text(
+            GROUP.format(user.first_name, context.bot.first_name),
+            parse_mode=ParseMode.HTML,
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=reply_markup1
+        )
+
+async def forward(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat = update.effective_chat
+    message = update.effective_message
+    user = update.effective_user
+    keyboard = [
+        [
+            InlineKeyboardButton("Update Channel", url="https://t.me/Star_Bots_Tamil"),
+            InlineKeyboardButton("Add My Channels", url="https://t.me/TG_Karthik")
+        ]
+    ]
+    keyboard1 = [
+        [
+            InlineKeyboardButton("Check Bot PM", url="https://t.me/Auto_Forward_Star_Bot"),
+            InlineKeyboardButton("Add My Channels", url="https://t.me/TG_Karthik")
+        ],
+        [
+            InlineKeyboardButton("Update Channel", url="https://t.me/Star_Bots_Tamil")
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    reply_markup1 = InlineKeyboardMarkup(keyboard1)
+    if not (chat and message and user):
+        return
+
+    if chat.type == "private":
+        await message.reply_text(
+            FORWARD.format(user.first_name, context.bot.first_name),
+            parse_mode=ParseMode.HTML,
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=reply_markup
+        )
+    else:
+        await message.reply_text(
+            GROUP.format(user.first_name, context.bot.first_name),
+            parse_mode=ParseMode.HTML,
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=reply_markup1
+        )
+
+bot.add_handler(CommandHandler("start", start))
+bot.add_handler(CommandHandler("help", help))
+bot.add_handler(CommandHandler("about", about))
+bot.add_handler(CommandHandler("forward", forward))
